12e982cba760db0176c2d7ac822f79377158a197
Jack Toumey <jack@toumey.io>
2025-09-28 11:50:59 -0700

    getting started with account and bank

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[derive(Debug)]
+struct Account {
+  id: u32,
+  balance: i32,
+  holder: String,
+
+}
+
+impl Account {
+  fn new(id: u32, holder: String) -> Self {
+    return Account { id, holder, balance: 0 };
+  }
+}
+
+#[derive(Debug)]
+struct Bank {
+  accounts: Vec<Account>,
+}
+
+impl Bank {
+  fn new() -> Self {
+    return Bank { accounts: vec![] };
+  }
+}
+
+fn print_account(account: Account) {
+  println!("{:#?}", account);
+}
+
 fn main() {
-  println!("Hello, world!");
+  let bank = Bank::new();
+  let account = Account::new(1, String::from("Jack"));
+  println!("{:#?}", bank);
+  print_account(account);
+  // print_account(account);
 }
